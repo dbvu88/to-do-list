e9e3d9daad3b158d5d375aeb1a90222c42c601ea
Duc Vu <ducvu@Ducs-MacBook-Pro.local>
2018-07-07 19:38:03 -0500

    initial commit

---
+++ src/Config/config.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+export var DB_CONFIG = {
+  apiKey: "AIzaSyBATI7ItmDQ1XfwSewAdeSglw3RQGfdT3k",
+  authDomain: "react-notes-21ac4.firebaseapp.com",
+  databaseURL: "https://react-notes-21ac4.firebaseio.com",
+  projectId: "react-notes-21ac4",
+  storageBucket: "",
+  messagingSenderId: "970202950457"
+};
+  
